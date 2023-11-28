9671fe036303761a66d153edf26f6ab33277d56f
apustovoitova <anastasiapustovoitova@gmail.com>
2023-11-28 10:45:25 +0100

    added this.classes, this.element  = "menu__item"

--- js/script.js
+++ js/script.js
@@ -139,12 +139,13 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // USING CLASSES FOR CARDS
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
       this.changeToUAH();
@@ -156,8 +157,16 @@ window.addEventListener("DOMContentLoaded", () => {
 
     render() {
       const element = document.createElement("div");
+      if (this.classes.length === 0) {
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach((className) =>
+          SVGFEFuncAElement.classList.add(className)
+        );
+      }
+      this.classes.forEach((className) => element.classList.add(className));
       element.innerHTML = `
-            <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -166,7 +175,6 @@ window.addEventListener("DOMContentLoaded", () => {
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
-            </div>
         `;
       this.parent.append(element);
     }
